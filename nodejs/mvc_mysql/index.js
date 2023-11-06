d2af75115248d9fc4d964b5534b4eba5ca00ad6e
JSHWJ <bogum1029@gmail.com>
2023-11-06 13:12:31 +0900

    feat: mvc_mysql

--- nodejs/mvc_mysql/index.js
+++ nodejs/mvc_mysql/index.js
@@ -1,22 +1,24 @@
-const express = require("express");
+const express = require('express');
 const app = express();
 const PORT = 8000;
 
-app.set("view engine", "ejs");
-app.set("views", "./views");
+app.set('view engine', 'ejs');
+app.set('views', './views');
 app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
-app.use("/static", express.static(__dirname + "/static"));
 
-//router
-const userRouter = require("./routes/user");
-app.use("/user", userRouter);
+//router 분리
+app.get('/', (req, res) => {
+    res.render('index');
+});
+const router = require('./routes');
+app.use('/visitor', router);
 
-//404
-app.use("*", (req, res) => {
-  res.render("404");
+//오류처리
+app.use('*', (req, res) => {
+    res.render('404');
 });
 
 app.listen(PORT, () => {
-  console.log(`http://localhost:${PORT}`);
+    console.log(`http://localhost:${PORT}`);
 });
